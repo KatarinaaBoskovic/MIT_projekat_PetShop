c248b53f9b35bae8f9e0db5718ebdd3630587a9b
KatarinaaBoskovic <katarinaboskovic65@gmail.com>
2026-02-07 18:41:47 +0100

    Wishlist ekran

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -28,28 +28,32 @@ final List<Product> products = [
     description:
         'Snooza Ortho Sofa is designed for older dogs who find it difficult to get in and out of their regular beds or baskets. The comfortable and durable Ortho Sofa has a secure orthopaedic support foam base that is stable and comfortable, as well as soft padding that provides a sense of comfort and calm for your pet.',
   ),
-   const Product(
+  const Product(
     name: 'Essential Dog Hoodie',
     category: 'Clothes',
     price: 2300,
     oldPrice: 3700,
     imageUrl: 'assets/images/product3.jpg',
-    description:'Signature Spark Paws Butterstretch™ fabric. Very stretchy, durable, soft texture. Fluffy cozy fleece interior. Machine wash cold, line dry. 55% Cotton, 40% Polyester, 5% Spandex',
+    description:
+        'Signature Spark Paws Butterstretch™ fabric. Very stretchy, durable, soft texture. Fluffy cozy fleece interior. Machine wash cold, line dry. 55% Cotton, 40% Polyester, 5% Spandex',
   ),
-   const Product(
+  const Product(
     name: 'Zolux Wild Rope',
     category: 'Toys',
     price: 1030,
     oldPrice: 1700,
+    isFavorite: true,
     imageUrl: 'assets/images/product2.jpg',
-    description:'Made from beech wood and 100% cotton rope',
+    description: 'Made from beech wood and 100% cotton rope',
   ),
-   const Product(
+  const Product(
     name: 'Orijen Puppy',
     category: 'Food',
     price: 13500,
     oldPrice: 15600,
+    isFavorite: true,
     imageUrl: 'assets/images/products2.jpg',
-    description:'Orijen Puppy provides biologically appropriate nutrition for puppies, with an emphasis on a high protein content derived from fresh chicken, turkey, fish, and chicken offal.',
-      ),
+    description:
+        'Orijen Puppy provides biologically appropriate nutrition for puppies, with an emphasis on a high protein content derived from fresh chicken, turkey, fish, and chicken offal.',
+  ),
 ];
